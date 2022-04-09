f08ab716afbe23fb0b2bee5f50161780b2bc1812
Caique Santos <caiquebsan@gmail.com>
2022-04-08 21:50:26 -0300

    Criacao da aplicacao com uso do expo

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class extends React.Component{
+
+  constructor (props){
+    super(props)
+    this.state = {
+      boaVinda: "Seja bem vindo ao app Gerador de Jogo Mega Sena!",
+      numeros: []
+    }
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>{this.state.boaVinda}</Text>
+        <Text>{this.state.numeros}</Text>
+        <Button
+          title="Gerar"
+          onPress={() => this.setState({
+            cont: this.state.cont + 1
+          })}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
